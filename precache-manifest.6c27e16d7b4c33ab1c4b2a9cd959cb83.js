032b321ad3024c36c20c822415fdf4a81e1714f1
dipuchaudhary <dipuchaudhary8@gmail.com>
2020-05-15 18:16:09 +0545

    Updates

---
+++ precache-manifest.6c27e16d7b4c33ab1c4b2a9cd959cb83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5077498fd04dd31716ee8a8a5f84d8",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "86de6eb358ff70401f51",
+    "url": "/FaceRecognitionApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a861a93dca643b849620",
+    "url": "/FaceRecognitionApp/static/css/main.6c093424.chunk.css"
+  },
+  {
+    "revision": "86de6eb358ff70401f51",
+    "url": "/FaceRecognitionApp/static/js/2.2fac2fb9.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/FaceRecognitionApp/static/js/2.2fac2fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a861a93dca643b849620",
+    "url": "/FaceRecognitionApp/static/js/main.4148bae9.chunk.js"
+  },
+  {
+    "revision": "9d4fea97eda359ad8e69",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.d0b1000e.js"
+  }
+]);
